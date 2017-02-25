f370f2da8767334f5e8425aaea91c58fb65292b0
Gonzo <Gonzo.Dark@gmail.com>
2017-02-25 13:07:25 +0100

    first commit

---
+++ SmallConsoleDemo/SmallConsoleDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello Cloud are you able to see this?
+        }
+    }
+}
